6d7affdac64feb59f7abcbe2fbf8948483ee6ad8
Aikanwha <dalton.wettlaufer@gmail.com>
2024-10-05 22:42:18 -0600

    created steamgrabber.py to make a list of steam games by tag, and put that data into gamelist.html as a table. Doesn't quite put it into a table yet.

--- steamgrabber.py
+++ steamgrabber.py
@@ -1,10 +1,10 @@
+#!/usr/bin/env python3
 #Goal is to get all the roguelike games from my steam library, get the name, release date, rating, my hours played,
 #and price on steam, and display it in a table on my web page.
-#Steam Web API: 
-#GetOwnedGames: returns appID and game name.
-#GetTagList: enter appID, return tag list, check for tag:'roguelike'
+
 import http.client
 import json
+import operator
 #Set up http client to steam web api
 steamHost = "api.steampowered.com"
 steamConn = http.client.HTTPSConnection(steamHost)
@@ -26,46 +26,90 @@ response = steamConn.getresponse()
 print(response.status, response.reason)
 gamesOwnedResponse = json.loads(response.read())
 
-def writeIfRoguelike(gameinfo,file):
+#TODO: filter games by hrs played, print to file the list of games in that order.
+#CHALLENGE: Make this take in any kind of game tag, and grab that instead of just roguelikes. and accept an amount from CLI
+class Game:
+    def __init__(self, game_info, spy_response):
+        self.icon = "http://media.steampowered.com/steamcommunity/public/images/apps/{APPID}/{IMG_ICON_URL}.jpg"
+        self.icon = self.icon.format(APPID=game_info["appid"], IMG_ICON_URL=game_info["img_icon_url"])
+        self.title = game_info["name"]
+        self.score = (1-(spy_response["negative"]/(spy_response["positive"]+spy_response["negative"])))*100
+        self.hrs_played = (game_info["playtime_windows_forever"] + game_info["playtime_mac_forever"] + game_info["playtime_linux_forever"] + game_info["playtime_deck_forever"])
+        self.hrs_played = (self.hrs_played / 60)
+        self.price = int(spy_response["price"])/100
+
+
+def add_if_has_tag(gameinfo,list):
     appID = gameinfo["appid"]
     #Request tag list from a appID
     tagListURL = "/api.php?request=appdetails&appid={}"
     tagListURL = tagListURL.format(appID)
     spyConn.request("GET", tagListURL, headers={"Host": spyHost})
     response = spyConn.getresponse()
-    #print(response.status, response.reason)
-    spyResponse = json.loads(response.read())
+    print(response.status, response.reason)
+    try:
+        spyResponse = json.loads(response.read())
+    except:
+        return
+    
 
     for x in spyResponse["tags"]:
-        if x == "Rogue-like":
-            score = spyResponse["negative"]/(spyResponse["positive"]+spyResponse["negative"])
-            score = (1-score)*100
-            file.write(
-                """
-                <tr>
-                <td>{icon}</td>
-                <td>{gametitle}</td>
-                <td>{score}</td>
-                <td>{hrsplayed}</td>
-                <td>{price}</td>
-                </tr>
-                """.format(icon="",gametitle=gameinfo["name"], score=score, hrsplayed=gameinfo["playtime_windows_forever"], price=spyResponse["price"])
-            )
+        if x == desired_tag:
+            list.append(Game(gameinfo, spyResponse))
             return
-file = open("gamelist.html", "w")
+
+grabbed_games = []
+desired_tag = "Rogue-like"
+num_games_wanted = 0
+
+print("Make sure to spell tags correctly or this won't work. For a full list of tags go to: 'https://steamdb.info/tags/'")
+desired_tag = input("What tag would you like to sort by? ")
+num_games_wanted = int(input("How many games would you like to get? (Games are sorted by highest hours played descending.) "))
+
 for x in gamesOwnedResponse["response"]["games"]:
-    writeIfRoguelike(x, file)
+    length = len(grabbed_games)
+    add_if_has_tag(x, grabbed_games)
+    if length != len(grabbed_games):
+        print("added: ", grabbed_games[length].title)
 
-#file = open("gamelist.html", "w")
-#check game for roguelike tag, if it has that tag, add to a list of games.
-#for x in spyResponse["tags"]:
-#    
-        
+grabbed_games.sort(key=operator.attrgetter("hrs_played"), reverse=True)
+file = open("gamelist.html", "w")
+if num_games_wanted < 1:
+    for game in grabbed_games:
+        file.write(
+        """
+        <tr>
+        <td><img src="{icon}" class="table-icon" alt="{gametitle} icon"/></td>
+        <td>{gametitle}</td>
+        <td>{score}</td>
+        <td>{hrsplayed}</td>
+        <td>{price}</td>
+        </tr>
+        """.format(
+                icon = game.icon, 
+                gametitle = game.title,
+                score = f"{game.score:.1f}", 
+                hrsplayed = f"{game.hrs_played:.2f}",
+                price = f"{game.price:.2f} USD"
+            )
+        )
+else:
+    for game in grabbed_games[:num_games_wanted]:
+        file.write (
+            """
+            <tr>
+            <td><img src="{icon}" class="table-icon" alt="{gametitle} icon"/></td>
+            <td>{gametitle}</td>
+            <td>{score}</td>
+            <td>{hrsplayed}</td>
+            <td>{price}</td>
+            </tr>
+            """.format(
+                    icon = game.icon, 
+                    gametitle = game.title,
+                    score = f"{game.score:.1f}", 
+                    hrsplayed = f"{game.hrs_played:.2f}",
+                    price = f"{game.price:.2f} USD"
+                )
+        )
 
-
-
-#grab Title,IconIMG Rating, MyHoursPlayed, Price.
-
-#add game info to dictionary
-
-#somehow display this information as a table.
